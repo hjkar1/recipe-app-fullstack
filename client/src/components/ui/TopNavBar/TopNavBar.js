6576797d44f6f67fc062b29ead4129ccad1c67dd
hjkar1 <hjkar1@outlook.com>
2019-11-06 18:33:45 +0200

    add navigation menu for small devices

--- client/src/components/ui/TopNavBar/TopNavBar.js
+++ client/src/components/ui/TopNavBar/TopNavBar.js
@@ -4,10 +4,13 @@ import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/core/styles';
 import { NavLink } from 'react-router-dom';
 import Link from '@material-ui/core/Link';
 import Button from '@material-ui/core/Button';
+import TopNavMenu from './TopNavMenu';
 import { logout } from '../../../store/actions/users';
 
 const useStyles = makeStyles(theme => ({
@@ -17,32 +20,49 @@ const useStyles = makeStyles(theme => ({
   },
   authLink: {
     color: 'inherit',
-    marginRight: theme.spacing(1),
-    marginLeft: theme.spacing(2)
+    marginRight: theme.spacing(2),
+    marginLeft: theme.spacing(2),
+    [theme.breakpoints.down('sm')]: {
+      display: 'none'
+    }
   },
   navLink: {
     color: 'inherit',
-    marginRight: theme.spacing(2),
-    marginLeft: theme.spacing(2)
+    marginRight: theme.spacing(1),
+    marginLeft: theme.spacing(2),
+    [theme.breakpoints.down('sm')]: {
+      display: 'none'
+    }
   },
   grow: {
     flexGrow: 1
   },
   active: {
     cursor: 'default',
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block'
+    fontWeight: 'bold'
+  },
+  menuButton: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none'
     }
   }
 }));
 
 export const TopNavBar = ({ children, logout }) => {
   const [loggingOut, setLoggingOut] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
 
   const handleLogout = () => {
     logout();
     setLoggingOut(true);
+  };
+
+  const handleMenuOpen = ({ currentTarget }) => {
+    setAnchorEl(currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
   };
 
   const classes = useStyles();
@@ -61,7 +81,7 @@ export const TopNavBar = ({ children, logout }) => {
       to="/"
       exact
     >
-      Search
+      Recipes
     </Link>
   );
 
@@ -91,6 +111,7 @@ export const TopNavBar = ({ children, logout }) => {
   const unregisteredUserLinks = (
     <Fragment>
       <Link
+        activeClassName={classes.active}
         underline="none"
         component={NavLink}
         className={classes.authLink}
@@ -99,6 +120,7 @@ export const TopNavBar = ({ children, logout }) => {
         Login
       </Link>
       <Link
+        activeClassName={classes.active}
         underline="none"
         component={NavLink}
         className={classes.authLink}
@@ -126,6 +148,22 @@ export const TopNavBar = ({ children, logout }) => {
           {children}
           {loggedIn ? recipeManagementLinks : null}
           {loggedIn ? logoutButton : unregisteredUserLinks}
+          <IconButton
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Menu button"
+            aria-owns={anchorEl ? 'menu' : undefined}
+            aria-haspopup="true"
+            onClick={handleMenuOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+          <TopNavMenu
+            anchorEl={anchorEl}
+            loggedIn={loggedIn}
+            handleClose={handleMenuClose}
+            handleLogout={handleLogout}
+          />
         </Toolbar>
       </AppBar>
     </div>
